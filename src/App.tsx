86defa4bd5b916cff6f0b67d025a64b445295142
Pablo Ortuño <pablo@buscoextra.es>
2020-04-30 13:49:20 +0200

    Added link to github

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,16 @@ function App() {
           <AppRouter></AppRouter>
         </div>
       </Router>
+      <div className="github">
+        <a href="https://github.com/oxigenao/react-wish-list">
+          <img
+            width="30px"
+            height="30px"
+            alt="logo-github"
+            src="https://cdn.worldvectorlogo.com/logos/github-icon.svg"
+          ></img>
+        </a>
+      </div>
     </div>
   );
 }
